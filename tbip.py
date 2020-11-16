a823c3886c778c4c63a0db40abc8e9de0308b8b2
sourav-ad <74101403+sourav-ad@users.noreply.github.com>
2020-11-16 10:01:03 +0100

    Update tbip.py

--- tbip.py
+++ tbip.py
@@ -131,7 +131,7 @@ def build_input_pipeline(data_dir,
       dense_shape=shuffled_counts.shape)
   dataset = tf.data.Dataset.from_tensor_slices(
       (documents, shuffled_counts, shuffled_author_indices))
-  batches = dataset.repeat().batch(batch_size).prefetch(batch_size)
+  batches = dataset.batch(batch_size)
   iterator = tf.data.batches.iter()
   #iterator = batches.make_one_shot_iterator()
   vocabulary = np.loadtxt(os.path.join(data_dir, "vocabulary.txt"), 
